b5cdd6cae3e9f9097420ea9e7028b2f0ae9e73ed
rajdeva2009 <44439160+rajdeva2009@users.noreply.github.com>
2020-06-01 20:26:10 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,28 @@
-def call(Map stageParams) {
+// def call(Map stageParams) {
+// 
+//    checkout([
+//        $class: 'GitSCM',
+//        branches: [[name:  stageParams.branch ]],
+//        userRemoteConfigs: [[ url: stageParams.url ]]
+//    ])
+//  }
 
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
-  }
+import com.google.common.collect.*
+    
+def call(args) {
+    
+    try {
+        def jobUrl = env.JOB_URL
+        println jobUrl
+        long startTime = System.currentTimeMillis()
+        println startTime
+        
+        def scm = steps.checkout args
+		def commitHash = scm.GIT_COMMIT
+        
+        }catch (exc) {
+		   recordEvent('anomaly', ['type':'GitCheckout','level':'error','where':'gitCheckout.groovy'])
+		   throw exc
+	       }
+   }
+        
